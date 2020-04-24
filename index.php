9c536e26336c42d8882fd0f51d5de64ecce2c7e5
Girish Gupta <girish@girish-gupta.com>
2020-04-24 13:38:23 +0530

    Deals with https://musicgoeson.com

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php
 
+if (substr($_SERVER['HTTP_HOST'],0,3)!="www")
+{
+  header("Location: https://www.".$_SERVER['HTTP_HOST'].$_SERVER[REQUEST_URI]);
+}
+
 switch (@parse_url($_SERVER['REQUEST_URI'])['path'])
 {
     case '/':
