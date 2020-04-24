5a61287e4fac6c26117121992d04cd0daa4ad3d2
Girish Gupta <girish@girish-gupta.com>
2020-04-24 13:32:50 +0530

    Completes front controller

--- index.php
+++ index.php
@@ -29,6 +29,108 @@ switch (@parse_url($_SERVER['REQUEST_URI'])['path'])
     case '/stats.php':
         require 'stats.php';
         break;
+    case '/news':
+        require 'news/index.php';
+        break;
+    case '/news/':
+        require 'news/index.php';
+        break;
+    case '/news/archive.php':
+        require 'news/archive.php';
+        break;
+    case '/reviews/albums':
+        require 'reviews/albums/index.php';
+        break;
+    case '/reviews/albums/':
+        require 'reviews/albums/index.php';
+        break;
+    case '/reviews/singles':
+        require 'reviews/singles/index.php';
+        break;
+    case '/reviews/singles/':
+        require 'reviews/singles/index.php';
+        break;
+    case '/artists':
+        require 'artists/index.php';
+        break;
+    case '/artists/':
+        require 'artists/index.php';
+        break;
+    case '/artists/artist.php':
+        require 'artists/artist.php';
+        break;
+    case '/artists/au_vi.php':
+        require 'artists/au_vi.php';
+        break;
+    case '/artists/az.php':
+        require 'artists/az.php';
+        break;
+    case '/artists/interviews.php':
+        require 'artists/interviews.php';
+        break;
+    case '/artists/live.php':
+        require 'artists/live.php';
+        break;
+    case '/artists/news.php':
+        require 'artists/news.php';
+        break;
+    case '/artists/reviews.php':
+        require 'artists/reviews.php';
+        break;
+    case '/competitions':
+        require 'competitions/index.php';
+        break;
+    case '/competitions/':
+        require 'competitions/index.php';
+        break;
+    case '/contactus':
+        require 'contactus/index.php';
+        break;
+    case '/contactus/':
+        require 'contactus/index.php';
+        break;
+    case '/forums':
+        require 'forums/index.php';
+        break;
+    case '/forums/':
+        require 'forums/index.php';
+        break;
+    case '/forums/add_edit.php':
+        require 'forums/add_edit.php';
+        break;
+    case '/forums/edit.php':
+        require 'forums/edit.php';
+        break;
+    case '/forums/forum.php':
+        require 'forums/forum.php';
+        break;
+    case '/forums/login.php':
+        require 'forums/login.php';
+        break;
+    case '/forums/profile.php':
+        require 'forums/profile.php';
+        break;
+    case '/forums/register.php':
+        require 'forums/register.php';
+        break;
+    case '/forums/topic.php':
+        require 'forums/topic.php';
+        break;
+    case '/interviews':
+        require 'interview/index.php';
+        break;
+    case '/interviews/':
+        require 'interview/index.php';
+        break;    
+    case '/live':
+        require 'live/index.php';
+	  break;
+    case '/live/':
+        require 'live/index.php';
+        break;    
+    case '/live/search.php':
+        require 'live/search.php';
+        break;    
     default:
         http_response_code(404);
         exit('Not Found');
